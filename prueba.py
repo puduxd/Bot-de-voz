5ed0e0dc7e8e7cf2574ff34e612abae72bcd9c0a
puduxd <14.ezequielgarces@gmail.com>
2021-05-13 11:27:53 -0400

    Mejora reconocer palabra bot

--- prueba.py
+++ prueba.py
@@ -3,15 +3,13 @@ import webbrowser
 import pyttsx3 as bot
 import pyautogui as gui
 import os
-
+from os import system
 
 jarvis = bot.init()
 jarvis.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0')
 jarvis.setProperty('rate',150)
 
-
 r = sr.Recognizer() 
-
 
 def pregunta():
     jarvis.say('¿Quieres buscar otra cosa?')
@@ -19,7 +17,7 @@ def pregunta():
     print('Escuchando : ')
     audio = r.listen(source)
     text = r.recognize_google(audio)
-    if text.lower()=="yes":
+    if text.lower()=="yes" or text.lower()=="si":
         jarvis.say('OK, ¿Qué quieres buscar?')
         jarvis.runAndWait()
         return True
@@ -28,7 +26,13 @@ def pregunta():
 
 
 with sr.Microphone() as source:
-    jarvis.say('Hola Ezequiel, ¿Qué quieres ver?')
+
+    jarvis.say('Bienvenido al asistente virtual creado por Pudú. Mi nombre es Jenny. vamos a empezar. ¿Cuál es tu nombre?')
+    jarvis.runAndWait() 
+    print('Escuchando nombre : ')
+    audio = r.listen(source)
+    name = r.recognize_google(audio)
+    jarvis.say('Hola' +name+'....¿Qué quieres ver?')
     jarvis.runAndWait() 
  
     while True:        
@@ -37,67 +41,69 @@ with sr.Microphone() as source:
         try:        
             text = r.recognize_google(audio)
             print('Usted dijo: {}'.format(text))
-            if text.lower()=="facebook":
+            if "facebook" in text.lower():
                 jarvis.say('Abriendo Facebook')
                 jarvis.runAndWait() 
-                webbrowser.open("http://www.facebook.com", new=2, autoraise=True)    
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://es-la.facebook.com/' )") 
+                
+             
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break
-            elif text.lower()=="twitter":
+            elif "twitter" in text.lower():
                 jarvis.say('Abriendo Twitter')
                 jarvis.runAndWait()
-                webbrowser.open("http://www.twitter.com", new=2, autoraise=True)
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://twitter.com/?lang=es' )") 
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break
-            elif text.lower()=="whatsApp":
+            elif "whatsApp" in text.lower():
                 jarvis.say('Abriendo Whatsapp')
                 jarvis.runAndWait()
-                webbrowser.open("https://web.whatsapp.com/", new=2, autoraise=True)   
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://web.whatsapp.com/' )")   
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break   
-            elif text.lower()=="instagram":
+            elif "instagram" in text.lower():
                 jarvis.say('Abriendo Instagram')
                 jarvis.runAndWait()
-                webbrowser.open("https://www.instagram.com/?hl=es-la", new=2, autoraise=True)   
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.instagram.com/?hl=es-la' )") 
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break  
-            elif text.lower()=="youtube":
+            elif "youtube" in text.lower():
                 jarvis.say('Abriendo Youtube')
                 jarvis.runAndWait()
-                webbrowser.open("https://www.youtube.com/", new=2, autoraise=True)   
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.youtube.com/' )")  
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break   
-            elif text.lower()=="spotify":
+            elif "spotify" in text.lower():
                 jarvis.say('Abriendo Spotify')
                 jarvis.runAndWait()
-                webbrowser.open("https://www.spotify.com/cl/home/", new=2, autoraise=True)   
+                system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.spotify.com/cl/' )") 
                 if pregunta()==True:                          
                     continue
                 else: 
                     jarvis.say('Ok, hasta pronto bebé')
                     jarvis.runAndWait() 
                     break  
-            elif text.lower()=="block":
+            elif "block" in text.lower():
                 os.system("notepad filename.txt")
                 if pregunta()==True:                          
                     continue
@@ -106,7 +112,7 @@ with sr.Microphone() as source:
                     jarvis.runAndWait() 
                     break
             
-            elif text.lower()=="exit":
+            elif "exit" in text.lower() or "no" in text.lower():
                 jarvis.say('Ok, Chao Bebé')
                 jarvis.runAndWait() 
                 break              
